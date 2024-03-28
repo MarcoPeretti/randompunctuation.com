3c70ae8d37c4da82458a8a578acf99d0044368b7
marcoperetti <marco.peretti@gmail.com>
2024-03-28 11:46:00 +0100

    fixed sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }));
 
-  const routes = ['', '/blog', '/about', 'projects'].map(
+  const routes = ['', '/blog', '/about', '/projects'].map(
     (route) => ({
       url: `https://randompunctuation.com${route}`,
       lastModified: new Date().toISOString().split('T')[0],
