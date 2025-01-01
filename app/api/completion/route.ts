2e0cff820baaeec2f1ceb24b6759482bb1955e59
MarcoAtSmallpdf <69680319+MarcoAtSmallpdf@users.noreply.github.com>
2025-01-01 18:28:28 +0100

    updated to latest AI sdk

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -1,14 +1,9 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { NextResponse } from 'next/server';
+// import OpenAI from 'openai';
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
 import cv from 'marco_peretti.resume.json';
 import {samplePrompts} from 'lib/prompts'
 
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
- 
 const env = process.env.NODE_ENV
 
 // IMPORTANT! Set the runtime to edge
@@ -36,28 +31,20 @@ export async function POST(req: Request) {
 
     //console.log(prompt);
 
-    // Ask OpenAI for a streaming completion given the prompt
-    const response = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct',
-      max_tokens: 350,
+    const textStream = streamText({
+      model: openai('gpt-4-turbo'),
       temperature: 0.6,
-      stream: true,   
+      maxTokens: 350,
       prompt: `You are a helpful assistant who is tasked to answer questions about my resume as if you were The Oracle from The Matrix movie sharing her wisdom. Answer using the third person form and refer to him either as Marco or he and limit the answer to about 150 words. Base your answers on my resume and do your very best to answer any question. Question:${prompt}  Resume: ${JSON.stringify(cv)}. If the answer cannot be found in the resume, write "Sorry, cannot not answer that question. Check spelling, rephrase or simply submit again. chatGPT's behaviour is somewhat erratic"`,
-    
+      async onFinish({ text, toolCalls, toolResults, usage, finishReason }) {
+        // implement your own logic here, e.g. for storing messages
+        // or recording token usage
+      },
     });
-  
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(response);
-  
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
+
+    return textStream.toDataStreamResponse();
   } catch (error) {
-    // Check if the error is an APIError
-    if (error instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
-    } else {
-      throw error;
-    }
+    console.error('Error processing request:', error);
+    return new Response('Internal Server Error', { status: 500 });
   }
 }
