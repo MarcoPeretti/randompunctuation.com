7e79770e0d71a403ea2a4cb4869a052a2bc81871
Marco Peretti <marco.peretti@gmail.com>
2025-05-22 15:56:04 +0200

    updated framer-motion and next-mdx-remote

--- app/components/nav-item.tsx
+++ app/components/nav-item.tsx
@@ -3,7 +3,7 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 
 export default function NavItem({
   path,
